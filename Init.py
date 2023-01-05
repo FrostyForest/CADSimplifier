f55845756fea9d85d348c6af257f0c206d001919
SD <SD@PC-DYLANCHOU>
2023-01-05 15:09:29 +0800

    初始化样例

---
+++ Init.py
@@ -0,0 +1,2 @@
+# FreeCAD init script of the CADSimplifier module
+# (c) 2001 Juergen Riegel LGPL
---
+++ InitGui.py
@@ -0,0 +1,16 @@
+# CADSimplifier gui init module
+# (c) 2001 Juergen Riegel LGPL
+
+class CADSimplifierWorkbench (Workbench):
+    "CADSimplifier workbench object"
+    MenuText = "CADSimplifier"
+    ToolTip = "CADSimplifier workbench"
+
+    def Initialize(self):
+        # load the module
+        import CADSimplifierGui
+
+    def GetClassName(self):
+        return "CADSimplifierGui::Workbench"
+
+Gui.addWorkbench(CADSimplifierWorkbench())
